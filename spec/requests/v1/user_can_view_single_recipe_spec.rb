611205bf8584fe419e393bd87d7fbff1c08e7f73
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-11 21:51:34 +0100

    Add test cases to request spec.

--- spec/requests/v1/user_can_view_single_recipe_spec.rb
+++ spec/requests/v1/user_can_view_single_recipe_spec.rb
@@ -1,3 +1,45 @@
 RSpec.describe 'GET specific recipe' do
   describe 'user can view a specific recipe' do
     let(:recipe) { create(:recipe) }
+    let(:headers) {{ HTTP_ACCEPT: "application/json" }
+
+    before do
+      get "/v1/recipes/#{recipe.id}"
+      headers: headers
+    end
+
+    it 'Recipe has a title' do      
+      expect(response_json['title']).to eq Recipe.first.title
+    end
+
+    it 'Recipe has content' do
+      expect(response_json['content']).to eq Recipe.first.content
+    end
+
+    it 'Recipe has directions' do
+      expect(response_json['directions']).to eq Recipe.first.content
+    end
+
+    it 'Returns a successful response status' do
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe 'Specific recipe not found' do
+    let(:recipe) { create(:recipe) }
+    let(:headers) {{ HTTP_ACCEPT: "application/json" }
+
+    before do
+      get "/v1/articles/12345"
+      headers: headers
+    end
+
+    it 'Returns an error status of 404' do
+      expect(response.status).to eq 404
+    end
+
+    it 'Returns an error message' do
+      expect(response_json["error_message"]).to eq "This recipe is currently unavailable."
+    end
+  end
+end
