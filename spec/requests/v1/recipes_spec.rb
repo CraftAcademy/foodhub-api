c377a8afe76d8981feb41cfefb11aeebe7fa7936
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-12 12:50:46 +0100

    -Adds recipe test response

--- spec/requests/v1/recipes_spec.rb
+++ spec/requests/v1/recipes_spec.rb
@@ -54,7 +54,8 @@ RSpec.describe 'V1::Recipes', type: :request do
         properties: {
           title: { type: :string },
           ingredients: { type: :string },
-          directions: { type: :string }
+          directions: { type: :string },
+          portions: { type: :integer },
         },
         required: %w[title ingredients directions]
       }
@@ -63,7 +64,8 @@ RSpec.describe 'V1::Recipes', type: :request do
         let(:recipe) do
           { title: 'Meatballs',
             ingredients: 'Minced meat, bacon, bread crumbs, cream, medium white chopped onion',
-            directions: 'In large bowl, place 3 lb lean ground beef, chopped 1 medium white onion, 3 tablespoons dried oregano leaves and 1/4 cup bread crumbs. Place large ovenproof skillet over medium-high heat; pour reserved bacon drippings into skillet. Add meatballs; cook about 3 minutes on each side or just until seared. (You may have to do this in batches.)' }
+            directions: 'In large bowl, place 3 lb lean ground beef, chopped 1 medium white onion, 3 tablespoons dried oregano leaves and 1/4 cup bread crumbs. Place large ovenproof skillet over medium-high heat; pour reserved bacon drippings into skillet. Add meatballs; cook about 3 minutes on each side or just until seared. (You may have to do this in batches.)' },
+            portions: '4'
         end
         run_test! do
           expect(response_json['message']).to eq 'The recipe was successfully created.'
@@ -74,7 +76,8 @@ RSpec.describe 'V1::Recipes', type: :request do
         let(:recipe) do
           { title: '',
             ingredients: '',
-            directions: '' }
+            directions: '',
+            portions: '' }
         end
         run_test! do
           expect(response_json['error_message']).to eq 'Unable to create recipe'
--- spec/requests/v1/user_can_view_single_recipe_spec.rb
+++ spec/requests/v1/user_can_view_single_recipe_spec.rb
@@ -1,3 +1,34 @@
 RSpec.describe 'GET specific recipe' do
   describe 'user can view a specific recipe' do
     let(:recipe) { create(:recipe) }
+    let(:headers) {{ HTTP_ACCEPT: "application/json" }
+
+    before do  
+      get "/api/v1/recipes/#{recipe.id}", headers: headers
+    end
+    
+    it 'returns status 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns the correct data' do
+      expected_response = {
+          "id"=>recipe.id,
+          "title"=>recipe.title,
+          "ingredients"=>recipe.ingredient,
+          "directions"=>recipe.direction,
+          "portions"=>recipe.portion,
+        }
+        expect(response_json['recipe']).to eq(expected_response)
+    end
+  end
+
+    describe 'The recipe could not be found' do
+      let(:headers) {{ HTTP_ACCEPT: "application/json" }
+      before do
+        get "/api/v1/recipes/#{recipe.id}", headers: headers
+      end
+
+      it 'returns the recipe could not be found' do
+        expect(response.status).to eq 404
+      end
