17fa86ca20f4837b899e8eeef46ee9493e084246
oliverochman <oliver.ochman@hotmail.com>
2019-11-13 15:20:40 +0100

    Refactor conditional to ternary operator
    Co-authored-by: Thomas Ochman <thomas@craftacademy.se>

--- app/services/decode_service.rb
+++ app/services/decode_service.rb
@@ -1,11 +1,7 @@
 module DecodeService
   def self.attach_image(file, target)
-    if Rails.env == 'test'
-      image = file
-    else
-      image = split_base64(file)
-    end
-
+    image = Rails.env.test? ? file : split_base64(file)
+    
     decoded_data = Base64.decode64(image[:data])
     io = StringIO.new
     io.puts(decoded_data)
