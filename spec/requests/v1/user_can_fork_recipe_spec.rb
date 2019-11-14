cafaa2eccc4c1f1b7d259cd2ff3c1621e6699cb2
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-14 18:57:17 +0100

    Add request spec for forking a recipe

---
+++ spec/requests/v1/user_can_fork_recipe_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Update specific recipe' do
+  let(:user_1) { create(:user) }
+  let(:user_2) { create(:user) }
+  let(:user_credentials) { user.create_new_auth_token }
+  let!(:valid_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+
+  let(:recipe) do
+    create(:recipe,
+           title: 'Cookies',
+           ingredients: 'Cookie ingredients, chocolate chips.',
+           directions: 'Make the cookies.'
+           user_id: user_1.id)
+  end
+  describe 'user can fork a recipe' do
+    let(:expected_attributes) do
+      {
+        title: 'Cookies',
+        ingredients: 'Cookie ingredients, chocolate chips.',
+        directions: 'Make the cookies.',
+        user_id: user_2.id
+      }
+    end
+    before do
+      post "/v1/recipes/#{recipe.id}/fork", headers: valid_headers
+    end
+
+    it 'returns 201 response' do
+      expect(response).to have_http_status 201
+    end
+
+    %i[title ingredients directions].each do |attribute|
+      it "updates #{attribute} as value on the last Recipe" do
+        expect(Recipe.last.send(attribute)).to eq expected_attributes[attribute]
+      end
+    end
+  end
