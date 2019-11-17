e858aef3b74513fb818081e9cc8dd6a85f804d40
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-17 22:26:09 +0100

    Add guard clause to serializers

--- app/serializers/recipes/index_serializer.rb
+++ app/serializers/recipes/index_serializer.rb
@@ -4,12 +4,12 @@ class Recipes::IndexSerializer < ActiveModel::Serializer
   attributes :id, :title, :ingredients, :directions, :image
 
   def image
-    if object.image.attached?
-      if Rails.env.test?
-        rails_blob_url(object.image)
-      else
-        object.image.service_url(expires_in: 1.hour, disposition: 'inline')
-      end
+    return nil unless object.image.attached?
+    
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
     end
   end
 end
--- app/serializers/recipes/show_serializer.rb
+++ app/serializers/recipes/show_serializer.rb
@@ -4,12 +4,12 @@ class Recipes::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :ingredients, :directions, :user_name, :user_id, :image
 
   def image
-    if object.image.attached?
-      if Rails.env.test?
-        rails_blob_url(object.image)
-      else
-        object.image.service_url(expires_in: 1.hour, disposition: 'inline')
-      end
+    return nil unless object.image.attached?
+
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
     end
   end
 
