8a9cf9fa12d1ec61757b79146c29a8d72b1b48a4
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-17 14:21:18 +0100

    Add image to Index serializer

--- app/serializers/recipes/index_serializer.rb
+++ app/serializers/recipes/index_serializer.rb
@@ -1,3 +1,13 @@
 class Recipes::IndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :ingredients, :directions
+  include Rails.application.routes.url_helpers
+  
+  attributes :id, :title, :ingredients, :directions, :image
+
+  def image
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    end
+  end
 end
