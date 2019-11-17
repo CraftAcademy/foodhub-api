3d71fdddac3138b840a5ce6f1434810244c82f4b
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-11-17 17:53:33 +0100

    Add attachment clause to image in serializers
    Applies to both index and show serializers.

--- app/serializers/recipes/show_serializer.rb
+++ app/serializers/recipes/show_serializer.rb
@@ -4,10 +4,12 @@ class Recipes::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :ingredients, :directions, :user_name, :user_id, :image
 
   def image
-    if Rails.env.test?
-      rails_blob_url(object.image)
-    else
-      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    if object.image.attached?
+      if Rails.env.test?
+        rails_blob_url(object.image)
+      else
+        object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+      end
     end
   end
 
