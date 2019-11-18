a3e16351adf0a40186382a2f3e517da43d0ec1d0
Thomas <thomas@craftacademy.se>
2019-11-18 19:26:42 +0100

    cleans comment from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
 
   has_many :recipes
   has_one :cookbook
-  has_many :favorites, through: :cookbook #, class_name: 'Recipe', foreign_key: :recipe_id
+  has_many :favorites, through: :cookbook
   has_many :favorite_recipes, through: :favorites, source: :recipe
 end
