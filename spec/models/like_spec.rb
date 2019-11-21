c754b2375b3834b570f8ac0c7b1318b396c5af57
clalil <cliljander@gmail.com>
2019-11-21 19:09:54 +0100

    Add model specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'DB columns' do
+    it { is_expected.to have_db_column :user_id }
+    it { is_expected.to have_db_column :recipe_id }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :recipe }
+  end
 end
