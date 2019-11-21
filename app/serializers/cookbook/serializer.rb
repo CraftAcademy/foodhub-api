eab57ef62211d6960dcc0d0428ce2a46c74535e0
clalil <cliljander@gmail.com>
2019-11-21 13:05:30 +0100

    Update CookBook serializer Extract image to own method, ann ternary for image attachment

--- app/serializers/cookbook/serializer.rb
+++ app/serializers/cookbook/serializer.rb
@@ -9,8 +9,18 @@ class Cookbook::Serializer < ActiveModel::Serializer
         id: favorite.recipe_id,
         title: favorite.recipe.title,
         user_name: favorite.recipe.user.name,
-        image: Rails.env === 'test' ? rails_blob_url(favorite.recipe.image) : favorite.recipe.image.service_url(expires_in: 1.hour, disposition: 'inline')
+        image: image(favorite)
       }
+    end
+  end
+
+  def image(favorite)
+    return nil unless favorite.recipe.image.attached?
+
+    if Rails.env === 'test'
+      rails_blob_url(favorite.recipe.image)
+    else
+      favorite.recipe.image.service_url(expires_in: 1.hour, disposition: 'inline')
     end
   end
 end
