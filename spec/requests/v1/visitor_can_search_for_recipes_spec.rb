5ad4bd4b46ca0278eb26fb8c2542e2ea9a56da1d
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-21 14:42:01 +0100

    [WIP] adds recipe count test.

--- spec/requests/v1/visitor_can_search_for_recipes_spec.rb
+++ spec/requests/v1/visitor_can_search_for_recipes_spec.rb
@@ -1,7 +1,10 @@
-RSpec.describe 'User can search for recipes', type: :request do
+RSpec.describe 'Visitor can search for recipes', type: :request do
       
-    before do
-        get '/v1/recipes', headers: headers
-      end
+  before do
+      get '/v1/recipes', headers: headers
+    end
+
+  it 'API returns 2 recipes' do
+    expect(response_json['recipes'].count).to eq 2
+  end
 end
-  
