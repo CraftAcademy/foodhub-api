2f67aa00e340f219cd36bef51889967023b81116
oliverochman <oliver.ochman@hotmail.com>
2019-11-21 16:29:39 +0100

    modify req spec

--- spec/requests/v1/visitor_can_search_for_recipes_spec.rb
+++ spec/requests/v1/visitor_can_search_for_recipes_spec.rb
@@ -1,10 +1,23 @@
 RSpec.describe 'Visitor can search for recipes', type: :request do
-      
-  before do
-      get '/v1/recipes', headers: headers
+  let!(:egg_recipe_1) { create(:recipe, title: 'Breakfast Egg Muffins') }
+  let!(:egg_recipe_2) { create(:recipe, ingredients: 'Eggs') }
+  let!(:non_egg_recipe) { create(:recipe, title: 'Sallad') }
+
+  describe "successfully" do
+    before do
+      get '/v1/search', 
+      params: {
+        search: 'Eggs'
+      }
+      headers: headers
     end
 
-  it 'API returns 2 recipes' do
-    expect(response_json['recipes'].count).to eq 2
+    it "returns 200 response" do
+      expect(response).to have_http_status 200
+    end
+    
+    it 'API returns 2 recipes' do
+      expect(response_json['recipes'].count).to eq 2
+    end
   end
 end
