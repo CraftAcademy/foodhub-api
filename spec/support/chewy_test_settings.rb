e1afb8dd96744becfea61a41ea645632985abdbc
Thomas <thomas@craftacademy.se>
2019-11-24 07:18:46 +0100

    restructures ES setup in tests

--- spec/support/chewy_test_settings.rb
+++ spec/support/chewy_test_settings.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 # NOTE! CI service (Semaphore) need to have env var CHEWY set to 'true'
 # This setup should work on localhost
 
 RSpec.configure do |config|
-  config.before(:suite) do 
-    Chewy.strategy(:bypass)
-    RecipesIndex.create! unless RecipesIndex.exists?
-  end
-  config.before(:each, type: :search_request) do
-    unless ENV['CHEWY']
+  unless ENV['CHEWY']
+    config.before(:suite) do
+      Chewy.strategy(:bypass)
+      RecipesIndex.create! unless RecipesIndex.exists?
+    end
+
+    config.before(:each, type: :search_request) do
       unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
         Elasticsearch::Extensions::Test::Cluster.start(
           port: 9250,
@@ -15,11 +18,10 @@ RSpec.configure do |config|
           timeout: 120
         )
       end
-      
     end
-  end
 
-  config.after(:each, type: :search_request) do
-    RecipesIndex.delete
+    config.after(:each, type: :search_request) do
+      RecipesIndex.delete
+    end
   end
 end
